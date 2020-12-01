be2b6d0c23ad6b0f421e60d510611afa208aaaa4
Alvin Motieram <amotieram99@yahoo.com>
2020-11-30 22:47:20 -0500

    Updates

---
+++ precache-manifest.fcf90d4a572feea197eb452e31e0bd6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b4e670478388068502298d90501387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1abcb2d5d31d98dfea7c",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "4136b358be2805f26f00",
+    "url": "/static/css/main.e0fa4061.chunk.css"
+  },
+  {
+    "revision": "1abcb2d5d31d98dfea7c",
+    "url": "/static/js/2.6af1f5d7.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.6af1f5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4136b358be2805f26f00",
+    "url": "/static/js/main.7a76e6d3.chunk.js"
+  },
+  {
+    "revision": "e02f93c7ed18bef049d6",
+    "url": "/static/js/runtime-main.81b0e0ca.js"
+  }
+]);
